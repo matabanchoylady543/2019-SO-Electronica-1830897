9f802c652ea751e645c2870aeeb717be22a665f7
diegosarria <sarria.diego@correounivalle.edu.co>
2019-06-14 21:18:31 -0500

    añadiendo threads.c mycommon.h

--- 2019_06_06/threads.c
+++ 2019_06_06/threads.c
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 #include "common.h"
 #include "common_threads.h"
+#include "mycommon.h"
+
+
 
 volatile int counter = 0; 
 int loops;
+int puerta;
 
 void *worker(void *arg) {
     int i;
     for (i = 0; i < loops; i++) {
+    	cerrar_puerta();
 	counter++;
+	abrir_puerta();
     }
     return NULL;
 }
@@ -21,12 +27,14 @@ int main(int argc, char *argv[]) {
     } 
     loops = atoi(argv[1]);
     pthread_t p1, p2;
+    crear_puerta(p1,p2);
     printf("Initial value : %d\n", counter);
     Pthread_create(&p1, NULL, worker, NULL); 
     Pthread_create(&p2, NULL, worker, NULL);
     Pthread_join(p1, NULL);
     Pthread_join(p2, NULL);
     printf("Final value   : %d\n", counter);
+    destruir_puerta();
     return 0;
 }
 
