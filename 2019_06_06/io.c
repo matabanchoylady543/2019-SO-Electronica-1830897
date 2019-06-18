9fc41bed5567d7f396ca755128fc3076025eb9e6
diegosarria <sarria.diego@correounivalle.edu.co>
2019-06-17 21:36:48 -0500

    añadiendo io.c

--- 2019_06_06/io.c
+++ 2019_06_06/io.c
@@ -1,35 +1,48 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <assert.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    //open es una funcion usada para apuntar a una ruta su primer parametro
-   //es una ruta, su segundo parametro es los permisos que utiliza
-    //su tercere parametro son los permisos que tendra el archivo
-    int fd = open("/tmp/file", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    //assert es una funcion que retorna true o false dependiendo de la condicion
-    //que reciba como primer parametro
-    assert(fd >= 0);
-    //se declara una variable char en el buffer con espacio para 20 bytes
-    char buffer[20];
-    //sprintf imprime una cadena la cual no saldra en el output sino que se guardara en el buffer
-    //en este caso hello world
-    sprintf(buffer, "%s", argv[1]);
-    //write escribe en un archivo, el primer parametro recibe un descriptor de archivo
-    //el segundo parametro recibe el buffer el cual contiene impreso el mensaje
-    //el tercer parametro recibe el tamaño de la cadena a escribir 
-    //strlen es una funcion que recibe una cadena en este caso la del buffer y retorna el tamaño de esta
-    int rc = write(fd, buffer, strlen(buffer));
-    assert(rc == (strlen(buffer)));
-    //fsync descarga en el archivo las intrucciones propuestas anteriormente usando la propia referencia
-    //en este caso fd
-    fsync(fd);
-    //cierra el archivo, recibe la referencia para cerrarlo
-    close(fd);
-    return 0;
+
+
+int main(int argc, char *argv[])
+{
+	char *archivo = argv[1];
+	FILE *file = fopen(archivo,"rb");
+	if (file == NULL)
+	{
+		printf("\n\nNo existe el archivo");
+		fclose(file);
+		exit(1);
+	}
+	else
+	{	
+		int cont =0;
+		fseek(file,0,SEEK_SET);
+		printf("\nTEXTO INGRESADO:\n");
+    	while (!feof(file))
+    	{	
+    		char letra;
+			letra = fgetc(file);
+			printf("%c",letra);
+			cont++;
+		}
+		fseek(file,-1,SEEK_END);
+		printf("\n\nTEXTO PROCESADO:\n");
+		int n = -1;
+		while (ftell(file)>=0)
+		{
+			char letra;
+			letra = fgetc(file);
+			printf("%c",letra);
+			n--;
+			if (n == -cont)
+			{
+				printf("\n\nPrograma finalizado\n");
+				return 0;
+			}
+			else 
+			{
+				fseek(file,n,SEEK_END);	
+			}
+		}
+	}
 }
-
